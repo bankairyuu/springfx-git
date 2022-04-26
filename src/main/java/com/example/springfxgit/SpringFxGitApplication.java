a383e9a0bb2770cc54667ccccfeade79350c8ade
Krisztian Sipos <bankairyuu@gmail.com>
2022-04-26 16:25:43 +0200

    init

---
+++ src/main/java/com/example/springfxgit/SpringFxGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfxgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFxGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFxGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springfxgit/SpringFxGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfxgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFxGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
